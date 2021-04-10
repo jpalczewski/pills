a71798253ad4781f57119e34a14456df2df3ee54
JP <jpalczewski@gmail.com>
2021-04-10 09:26:17 +0100

    ggzxc

--- daemon.py
+++ daemon.py
@@ -1,7 +1,8 @@
 import daemon, time, asyncio, aiocron
 from hardware.dht import poll_once
 from hardware.lcd.driver import update_LCD
-
+from config import config
+import pyrebase
 debugMode : bool = True
 
 async def debugLCD():
@@ -13,7 +14,9 @@ if debugMode:
     cron = aiocron.crontab('*/1 * * * *', func=debugLCD, start=True)
 
 
-    
+
+
+firebase = pyrebase.initialize_app(config)    
 
 asyncio.get_event_loop().run_forever()
     
